8dfe34047dc253afd2ad42b02913acb740ed4311
Christian <christian.kopac@gmail.com>
2020-11-07 20:00:39 +0100

    feat: improve 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,17 @@
+/// @jsx jsx
 import React from "react"
 
+import { Heading, Text, jsx } from "theme-ui"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Heading as="h1" sx={{ fontSize: "8em" }}>
+      404: Not Found
+    </Heading>
+    <Text>You just hit a route that doesn&#39;t exist... the sadness.</Text>
   </Layout>
 )
 
