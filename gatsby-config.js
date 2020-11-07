94952bc0ca4a9d6f8c9758e36008618f5e6f5fe0
Christian <christian.kopac@gmail.com>
2020-11-07 19:51:34 +0100

    feat: seo update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Christian Kopač - Code & Music`,
+    description: `My personal writings on code, music and more. JavaScript and TypeScript.`,
+    author: `@christiankopac`,
   },
   plugins: [
     {
