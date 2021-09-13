10b6144b40c5fad5737a8bdebcb8b15c94a7c732
Christian Kopač <hello@christiankopac.com>
2021-09-13 21:08:38 +0200

    updated defaults

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,9 +79,9 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        theme_color: `#663399`,
+        theme_color: `#F56542`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ck-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
